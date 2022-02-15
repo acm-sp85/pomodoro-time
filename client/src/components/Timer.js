2c0455165625eb707a78ec24aa479b3c6426589b
Alex-new-laptop <acontell@gmail.com>
2022-02-15 18:16:11 -0500

    adding first component

---
+++ client/src/components/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function timer() {
+  return (
+    <div className="main-container">
+      <h3>TIMER</h3>
+      <p>seconds</p>
+    </div>
+  );
+}
+
+export default timer;
