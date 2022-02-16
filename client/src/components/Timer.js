ff981b91ac51054f92dbd690575c63047aa854d5
Alex-new-laptop <acontell@gmail.com>
2022-02-16 18:29:53 -0500

    Added countdown functionality, counting down to 0 propperly

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function timer() {
+function Timer() {
+  let [seconds, setSeconds] = useState(2);
+  let [minutes, setMinutes] = useState(1);
+  let countDown = () => {
+    if (seconds > 0 && minutes >= 0) {
+      console.log(seconds);
+      setSeconds((seconds = seconds - 1));
+    } else if (minutes >= 1) {
+      setSeconds((seconds = 59));
+      setMinutes((minutes = minutes - 1));
+    }
+  };
   return (
     <div>
       <div className="main-container">
-        <h3 className="timer-numbers">25:00</h3>
-        <p className='start-button'>START</p>
+        <h3 className="timer-numbers">{`${minutes}:${seconds}`}</h3>
+        <p
+          className="start-button"
+          onClick={() => {
+            setInterval(countDown, 1000);
+          }}
+        >
+          START
+        </p>
       </div>
     </div>
   );
 }
 
-export default timer;
+export default Timer;
