b6c13f15bdab08778afe34f2fca3fa5ba8e8b092
Alex-new-laptop <acontell@gmail.com>
2022-02-16 19:02:41 -0500

    StartStop toggle conditional rendering works, timer still needs to follow

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 
 function Timer() {
-  let [seconds, setSeconds] = useState(2);
-  let [minutes, setMinutes] = useState(1);
+  let [seconds, setSeconds] = useState(11);
+  let [minutes, setMinutes] = useState(10);
+  let [toggle, setToggle] = useState(false);
   let countDown = () => {
     if (seconds > 0 && minutes >= 0) {
-      console.log(seconds);
-      setSeconds((seconds = seconds - 1));
-    } else if (minutes >= 1) {
-      setSeconds((seconds = 59));
-      setMinutes((minutes = minutes - 1));
+      if (seconds <= 10) {
+        setSeconds((seconds = '0' + (seconds - 1)));
+      } else {
+        setSeconds((seconds = seconds - 1));
+      }
+    } else if (minutes >= 1 && toggle) {
+      if (minutes <= 10) {
+        setSeconds((seconds = 59));
+        setMinutes((minutes = '0' + (minutes - 1)));
+      } else {
+        setSeconds((seconds = 59));
+        setMinutes((minutes = minutes - 1));
+      }
+    } else {
     }
   };
   return (
@@ -20,9 +30,10 @@ function Timer() {
           className="start-button"
           onClick={() => {
             setInterval(countDown, 1000);
+            setToggle((toggle = !toggle));
           }}
         >
-          START
+          {toggle ? 'STOP' : 'START'}
         </p>
       </div>
     </div>
