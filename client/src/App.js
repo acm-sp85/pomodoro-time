9d5238a099799fa4f31a5dd837d57cf02ac9365c
Alex-new-laptop <acontell@gmail.com>
2022-02-22 17:30:56 -0500

    Changes in the logic to switch between working time and break, not working propperly now

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,13 @@ import React, { useState } from 'react';
 import Settings from './components/Settings';
 
 function App() {
-  let [minutes, setMinutes] = useState('50');
+  let [minutes, setMinutes] = useState(1);
+  let [minutesBreak, setMinutesBreak] = useState(2);
   let [showModal, setShowModal] = useState(false);
   return (
     <div>
       {/* <h1 style={{ 'text-align': 'center' }} className="title">POMODORO TIME</h1> */}
-      <Timer minutes={minutes} />
+      <Timer minutes={minutes} minutesBreak={minutesBreak} />
       <p onClick={() => setShowModal(!showModal)} className="buttons">
         SETTINGS
       </p>
--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
 function Timer(props) {
-  let [seconds, setSeconds] = useState(props.seconds || '00');
-  let [minutes, setMinutes] = useState(props.minutes || '25');
+  let [seconds, setSeconds] = useState(0);
+  let [minutes, setMinutes] = useState(props.minutes || 0);
+  let [secondsBreak, setSecondsBreak] = useState(0);
+  let [minutesBreak, setMinutesBreak] = useState(props.minutesBreak);
+  let [toggleBreak, setToggleBreak] = useState(false);
   let [toggle, setToggle] = useState(false);
   let [startTimer, setStartTimer] = useState();
-  let countDown = () => {
+
+  let countDownPomodoro = () => {
     if (seconds > 0 && minutes >= 0) {
       if (seconds <= 10) {
         setSeconds((seconds = '0' + (seconds - 1)));
@@ -20,14 +24,51 @@ function Timer(props) {
         setSeconds((seconds = 59));
         setMinutes((minutes = minutes - 1));
       }
-    } else {
+    } else if (minutes == 0 && seconds == 0 && toggle && toggleBreak) {
+      setToggleBreak((toggleBreak = !toggleBreak));
+      setMinutes(props.minutes || '15');
+      setSeconds(59);
+      // setToggle((toggle = !toggle));
+      console.log('time for a break');
+
+      // setStartTimer(setInterval(countDownPomodoro, 1000));
+    } else if (minutes == 0 && seconds == 0 && toggle) {
+      setToggleBreak((toggleBreak = !toggleBreak));
+      setSeconds(59);
+      setMinutes(props.minutesBreak || '15');
     }
   };
 
+  // let countDownBreak = () => {
+  //   console.log('pedro');
+  //   if (seconds > 0 && minutes >= 0) {
+  //     if (seconds <= 10) {
+  //       setSeconds((seconds = '0' + (seconds - 1)));
+  //     } else {
+  //       setSeconds((seconds = seconds - 1));
+  //     }
+  //   } else if (minutes >= 1 && toggle) {
+  //     if (minutes <= 10) {
+  //       setSeconds((seconds = 59));
+  //       setMinutes((minutes = '0' + (minutes - 1)));
+  //     } else {
+  //       setSeconds((seconds = 59));
+  //       setMinutes((minutes = minutes - 1));
+  //     }
+  //   } else if (minutes == 0 && seconds == 0 && toggle) {
+  //     setMinutes(props.minutes || '15');
+  //     clearInterval(startTimer);
+  //     setToggle((toggle = !toggle));
+  //     setStartTimer(setInterval(countDownPomodoro, 1000));
+  //     console.log('time to work');
+  //   }
+  // };
+
+  // create a new toggle for pomodoro/break, if toggle is at break start the timer for the break
   const timer = () => {
     if (!toggle) {
       setToggle((toggle = !toggle));
-      setStartTimer(setInterval(countDown, 1000));
+      setStartTimer(setInterval(countDownPomodoro, 1000));
     } else {
       clearInterval(startTimer);
       setToggle((toggle = !toggle));
